817ef6acf8e871f15d9c02db8adafbe53a0e4544
Joseph Emanuele <josephemanuele@gmail.com>
2020-05-18 18:41:52 -0400

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-/* index.js */
+document.getElementById('login').addEventListener('click', () => {
+    document.getElementById('debug').innerHTML = 'Logging in...';
+    setTimeout(() => {
+        fetch('/user')
+            .then(res => res.json())
+            .then(data => {
+                document.getElementById('debug').innerHTML = data.name + ", " + data.age;
+            })
+            .catch(error => {
+                alert(error);
+            });
+    }, 500);
+});
