dc8c6ba6ae74fad8fec55bee403fdda2770abe8c
Joseph Emanuele <josephemanuele@gmail.com>
2020-05-21 22:29:55 -0400

    Gets and displays user's data via Spotify Web API

--- public/group.js
+++ public/group.js
@@ -1,9 +1,30 @@
-const ACCESS_TOKEN = 'midl-access-token';
+var SpotifyWebApi = require('../node_modules/spotify-web-api-js');
+var spotify = new SpotifyWebApi();
+
+function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    spotify.setAccessToken(getHashParams().access_token);
+    spotify.getMe()
+        .then(res => {
+            document.getElementById('debug').innerHTML = res.display_name;
+        })
+        .catch(err => {
+            document.getElementById('debug').innerHTML = err;
+        })
+
     document.getElementById('logout').addEventListener('click', () => {
-        localStorage.removeItem(ACCESS_TOKEN); 
         document.getElementById('debug').innerHTML = 'Logging out...';
-        window.location = '/index.html';
+        window.location = '/index.html'; 
     });
+
 })
--- public/index.js
+++ public/index.js
@@ -1,15 +1,8 @@
-const ACCESS_TOKEN = 'midl-access-token';
-
 document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem(ACCESS_TOKEN) !== null
-        /* && actual authorization */) {
-        window.location = '/group.html';
-    }
-    else {
-        localStorage.setItem(ACCESS_TOKEN, window.location.hash);    
-        document.getElementById('login').addEventListener('click', () => {
-            document.getElementById('debug').innerHTML = 'Logging in...';
-            window.location = '/login';
-        });
-    }
+
+    document.getElementById('login').addEventListener('click', () => {
+        document.getElementById('debug').innerHTML = 'Logging in...';
+        window.location = '/login';
+    });
+
 })
