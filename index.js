7edf2f84efe14454b571e22b63810c4e8780b000
Joseph Emanuele <josephemanuele@gmail.com>
2020-05-23 22:56:18 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    document.getElementById('login').addEventListener('click', () => {
-        document.getElementById('debug').innerHTML = 'Logging in...';
-        window.location = '/login';
-    });
-
-})
