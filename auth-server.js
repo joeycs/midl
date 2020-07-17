52a19af01f675f2983d9c49b42e11eca86387a55
Joseph Emanuele <josephemanuele@gmail.com>
2020-07-17 19:52:05 -0400

    Updated authentication scope

--- auth-server.js
+++ auth-server.js
@@ -45,7 +45,7 @@ app.get('/login', (req, res) => {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-read-playback-state';
+  var scope = 'user-read-private user-library-read user-read-playback-state playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
