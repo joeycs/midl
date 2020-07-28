9666955c76b055722b1bac4943e1fe5a3d29a479
Joseph Emanuele <josephemanuele@gmail.com>
2020-07-28 15:02:49 -0400

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,23 +1,3 @@
-const detectMobile = () => {
-    const toMatch = [
-        /Android/i,
-        /webOS/i,
-        /iPhone/i,
-        /iPad/i,
-        /iPod/i,
-        /BlackBerry/i,
-        /Windows Phone/i
-    ];
-
-    return toMatch.some((toMatchItem) => {
-        return navigator.userAgent.match(toMatchItem);
-    });
-}
-
-if (detectMobile()) {
-    window.location = "/mobile.html";
-}
-
 document.getElementById('hidden-header').style.color = "#e9e3d5";
 
 document.getElementById('login').addEventListener('click', () => {
