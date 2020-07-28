2f55b1c4ce609624e489183023da4a22c5586efa
Joseph Emanuele <josephemanuele@gmail.com>
2020-07-28 17:10:33 -0400

    Add files via upload

--- public/mobile.js
+++ public/mobile.js
@@ -1,1 +1,9 @@
 document.getElementById('hidden-header').style.color = "#e9e3d5";
+
+setTimeout(() => {
+    document.getElementById('hidden-image').style.color = "#e9e3d5";
+}, 1000);
+
+setTimeout(() => {
+    document.getElementById('hidden-paragraph').style.color = "#e9e3d5";
+}, 2000);
